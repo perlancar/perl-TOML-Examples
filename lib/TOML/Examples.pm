06d59e664c5a6bba71f82a831e203c4ad4cd2eca
perlancar (on pc-office) <perlancar@gmail.com>
2017-05-09 19:19:02 +0700

    Initial commit

---
+++ lib/TOML/Examples.pm
@@ -0,0 +1,17 @@
+package TOML::Examples;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Example TOML configuration files
+
+=head1 DESCRIPTION
+
+This distribution contains various examples of TOML configuration files in its
+per-dist share dir.
+
+
+=head1 SEE ALSO
+
+L<https://github.com/toml-lang/toml>
